ac585c091edaf19c53595567eee5fa953361e955
JN Kibira <33089347+97joseph@users.noreply.github.com>
2020-04-09 09:20:37 +0300

    Python crawler files

---
+++ index.py
@@ -0,0 +1,14 @@
+import requests
+from bs4 import BeautifulSoup
+def web(page,WebUrl):
+    if(page>0):
+        url = WebUrl
+        code = requests.get(url)
+        plain = code.text
+        s = BeautifulSoup(plain, "html.parser")
+        for link in s.findAll('a', {'class':'s-access-detail-page'}):
+            tet = link.get('title')
+            print(tet)
+            tet_2 = link.get('href')
+            print(tet_2)
+web(1,'https://cryptwerk.com/companies/shops')

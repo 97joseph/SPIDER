9f22bfea0b289eeb665f20f75c7a4a65449466b7
JN Kibira <33089347+97joseph@users.noreply.github.com>
2020-04-18 07:58:31 +0300

    refactor for the poor project design

--- Multicrawler/main.py
+++ Multicrawler/main.py
@@ -4,10 +4,12 @@ from spider import Spider
 from domain import *
 from demo import *
 PROJECT_NAME ='thesite'
-HOMEPAGE = 'your url goes here'
+HOMEPAGE = 'https://cryptwerk.com/companies'
+#HOMEPAGE='https://www.gooogle.com'
 DOMAIN_NAME = get_domain_name(HOMEPAGE)
 QUEUE_FILE = PROJECT_NAME + '/queue.txt'
 CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
+#* threads are sufficient
 NUMBER_OF_THREADS = 8
 queue= Queue()
 Spider(PROJECT_NAME,HOMEPAGE,DOMAIN_NAME)
@@ -37,3 +39,4 @@ def work():
         queue.task_done()
 create_workers()
 crawl()
+ 

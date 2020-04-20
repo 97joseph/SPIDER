15dfd88c40eb503aa814ec5cd93c4aef4c01411b
JN Kibira <33089347+97joseph@users.noreply.github.com>
2020-04-20 11:31:14 +0300

    final copy

--- Multicrawler/BaseMethods/monster.py
+++ Multicrawler/BaseMethods/monster.py
@@ -18,3 +18,21 @@ job_elems=results.find_all('section',class_='card-content')
 #Iterate over all of them
 for job_elem in job_elems:
     print(job_elem,end='\n'*2)
+    #Each job_elem is a new BeautifulSoup object
+    title_elem=job_elem.find('h1',class_='title')
+    company_elem=job_elem.find('div',class_='company')
+    location_elem=job_elem.find('div',class_='location')
+    if None in (title_elem,company_elem,location_elem):
+        continue
+    print(title_elem.text)
+print(company_elem.text)
+print(location_elem.text)
+print()
+        #may lack data due to capitalization
+         #python_jobs=results.find_all('h2',string='Python Developer')
+python_jobs=results.find_all('h2',string=lambda text:'python' in text.lower())
+print(len(python_jobs))
+for p_job in python_jobs:
+            link=p_job.find('a')['href']
+            print(p_job.text.strip())
+            print(f"Apply here:{link}\n")

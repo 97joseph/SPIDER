eb615159bc32854f6d7f569d6d6202c86a2c22f2
JN Kibira <33089347+97joseph@users.noreply.github.com>
2020-04-20 13:59:06 +0300

    debug errors

--- Python/FINAL_TEST/pandaspy2.py
+++ Python/FINAL_TEST/pandaspy2.py
@@ -15,22 +15,22 @@ from bs4 import BeautifulSoup
 chrome_path=r"C:\webdrivers\chromedriver\chromedriver.exe"
 driver = webdriver.Chrome(chrome_path)
 
-products=[] #List to store name of the product
-prices=[] #List to store price of the product
-ratings=[] #List to store rating of the product
+Name=[] 
+Description=[] 
+Image=[] 
 driver.get("https://cryptwerk.com/companies/shops/")
 
 content = driver.page_source
 soup = BeautifulSoup(content,"lxml")
-for a in soup.findAll('a',href=True, attrs={'class':'_31qSD5'}):
-                       name=a.find('div', attrs={'class':'_3wU53n'})
-                       price=a.find('div', attrs={'class':'_1vC4OE _2rQ-NK'})
-                       rating=a.find('div', attrs={'class':'hGSR34 _2beYZw'})
-                       products.append(name.text)
-                       prices.append(price.text)
-                       ratings.append(rating)
+for a in soup.findAll('a',href=True, attrs={'class':'media_body'}):
+                       Name=a.find('div', attrs={'class':'title'})
+                      # price=a.find('div', attrs={'class':'_1vC4OE _2rQ-NK'})
+                       #rating=a.find('div', attrs={'class':'hGSR34 _2beYZw'})
+                       Name.append(Name.text)
+                       #Description.append(price.text)
+                       #Image.append(rating)
 
 #store the data in the required format
 
-df = pd.DataFrame({'Product Name':products,'Price':prices,'Rating':ratings}) 
+df = pd.DataFrame({'Product Name':Name})#'Price':prices,'Rating':ratings}) 
 df.to_csv('products.csv', index=False, encoding='utf-8')
